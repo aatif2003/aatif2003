b5a58baaab79761d973537f65c58ef8b45e69712
aatif2003 <142872646+aatif2003@users.noreply.github.com>
2023-08-22 12:04:32 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+class Array {
+    public static void main(String args[]) {
+        int station[] = new int[5];
+        station[0] = 10;
+        station[1] = 5;
+        station[2] = 12;
+
+        for (int i = 0; i < station.length; i++) {
+            System.out.println(station[i]);
+
+        }
+
+    }
+}
